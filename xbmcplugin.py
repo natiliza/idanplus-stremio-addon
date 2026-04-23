aed792f792f711c241105b95f9b3febcf42205bc
natiliza <94228515+natiliza@users.noreply.github.com>
2026-04-23 23:02:26 +0300

    Add files via upload

---
+++ xbmcplugin.py
@@ -0,0 +1,21 @@
+SORT_METHOD_LABEL = 1
+
+
+def addDirectoryItem(*args, **kwargs):
+    return True
+
+
+def endOfDirectory(*args, **kwargs):
+    return True
+
+
+def addSortMethod(*args, **kwargs):
+    return True
+
+
+def setResolvedUrl(*args, **kwargs):
+    return True
+
+
+def setContent(*args, **kwargs):
+    return True
