991ede1ec07e46b1449279ec6493d24467b29d3b
natiliza <94228515+natiliza@users.noreply.github.com>
2026-04-23 22:08:36 +0300

    Add files via upload

---
+++ resources/lib/radio.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+import re
+import resources.lib.common as common
+
+module = 'radio'
+
+def WatchLive(channelID, name='', iconimage='', quality='best'):
+	channel = common.GetChannel(channelID)
+	isAdaptive = common.GetChannelAdaptive(channel)
+	linkDetails = channel.get('linkDetails')
+	userAgent = common.GetUserAgent()
+	headers={"User-Agent": userAgent}
+	regex = linkDetails.get('regex')
+	link = linkDetails['link']
+	if regex:
+		text = common.OpenURL(link, headers=headers)
+		link = re.compile(regex, linkDetails.get('flags', 0)).findall(text)[0]
+
+	if link.find('://') < 0:
+		link = 'http://' + link
+	
+	final = '{0}|User-Agent={1}&verifypeer=false'.format(link, userAgent)
+	common.PlayStream(final, quality, name, iconimage, adaptive=isAdaptive)
+
+def Run(name, url, mode, iconimage='', moreData=''):
+	if mode == 11:
+		WatchLive(url, name, iconimage, moreData)
+		
+	common.SetViewMode('episodes')
